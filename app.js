a1738d67cc8809e79d8717f017fcdeeb6d237003
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2023-08-29 17:06:19 +0530

    Changes done in app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+//Express Server
+const express = require("express");
+//express layout -- Creating Template
+const expressLayout = require("express-ejs-layoutss");
+
+//express app
+const app = express();
+const port = process.env.PORT || 3000;
+
+require("dotenv").config();
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+
+//static folder setup
+app.use(express.static("public"));
+
+//Layout
+app.use(expressLayout);
+
+//all layout store
+app.set("layout", "./layouts/main");
+
+// Routes
+const routes = require("./server/routes/recipeRoutes.js");
+
+app.use("/", routes);
+
+app.listen(port, (req, res) => {
+  console.log("App Started on Port:", port);
+});
