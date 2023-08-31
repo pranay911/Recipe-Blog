769e6dd484431c6130b799bdaa2412cf505f1693
Pranay Gaikwad <pranaygaikwad1432@gmail.com>
2023-08-31 19:22:49 +0530

    Categories Taken from DB

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -7,32 +7,49 @@ const Category = require("../models/Category");
  */
 
 exports.homepage = async (req, res) => {
-  res.render("index", { title: "Recipe Blog - Homepage" });
+  try {
+    //DB Query to Grad categories
+    //Only 5 categories can Display
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+    res.render("index", { title: "Recipe Blog - Homepage", categories });
+  } catch (error) {
+    // console.log(error);
+    res.send("An error Occured::");
+  }
 };
 
-async function insertDummyCategoryData() {
-  try {
-    await Category.insertMany([
-      {
-        name: "American",
-        image: "american-food.jpg",
-      },
-      {
-        name: "Chinese",
-        image: "chinese-food.jpg",
-      },
-      {
-        name: "Thai",
-        image: "thai-food.jpg",
-      },
-      {
-        name: "Mexican",
-        image: "mexican-food.jpg",
-      },
-    ]);
-  } catch (error) {
-    console.log(error);
-  }
-}
-insertDummyCategoryData();
+// async function insertDummyCategoryData() {
+//   try {
+//     await Category.insertMany([
+//       {
+//         name: "American",
+//         image: "american-food.jpg",
+//       },
+//       {
+//         name: "Chinese",
+//         image: "chinese-food.jpg",
+//       },
+//       {
+//         name: "Thai",
+//         image: "thai-food.jpg",
+//       },
+//       {
+//         name: "Mexican",
+//         image: "mexican-food.jpg",
+//       },
+//       {
+//         name: "Indian",
+//         image: "indian-food.jpg",
+//       },
+//       {
+//         name: "Spanish",
+//         image: "spanish-food.jpg",
+//       },
+//     ]);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// insertDummyCategoryData();
 // module.exports = insertDummyCategoryData;
